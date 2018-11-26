3ee402997da454de9e4c65756afc6b47989881e9
Trainee GSS <trainee@kgisl.com>
2018-11-26 14:55:19 +0530

    first commit

---
+++ src/main/java/com/kgisl/service/TeamService.java
@@ -0,0 +1,18 @@
+package com.kgisl.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.kgisl.entity.Team;
+
+
+
+public interface TeamService {
+
+    public Team createTeam(Team team);
+    public List<Team> getTeams();
+    public Team findByTeamId(Long id);
+    public Team updateTeam(Long id,Team team);
+    public void deleteTeamById(Long id); 
+
+}
