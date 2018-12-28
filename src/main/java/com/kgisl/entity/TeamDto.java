70ebc2739924ea67772147bc37b1579d2f925c81
Trainee GSS <trainee@kgisl.com>
2018-12-28 09:58:47 +0530

    files added

--- src/main/java/com/kgisl/entity/TeamDto.java
+++ src/main/java/com/kgisl/entity/TeamDto.java
@@ -1,12 +1,5 @@
 package com.kgisl.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 /**
  * Team
  */
